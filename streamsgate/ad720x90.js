4f89f0883281a20377921c074af574ff2018b9f0
streamer4uu <136008309+streamer4uu@users.noreply.github.com>
2023-06-08 11:01:11 -0700

    Add files via upload

---
+++ streamsgate/ad720x90.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '9f073b6eeb9f087b6abe12760925bff9',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bookshelfcomplaint.com/9f073b6eeb9f087b6abe12760925bff9/invoke.js"></scr' + 'ipt>');
